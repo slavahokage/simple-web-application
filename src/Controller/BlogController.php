cb1c6af50d3c9957b467f52f897cd81deb972a4b
slavahokage <v_smirnov@ikantam.com>
2019-08-14 17:21:59 +0300

    little fixs

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Model\Blogs;
-use Core\Router\Request;
 
 class BlogController extends Controller
 {
@@ -20,10 +19,5 @@ class BlogController extends Controller
         }
 
         return json_encode($blogs->getData());
-    }
-
-    public function show()
-    {
-        $this->request->sayHello();
     }
 }
