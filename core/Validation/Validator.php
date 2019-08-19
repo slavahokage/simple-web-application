59cba5f53533de4ca5d0d65579f86d06129b17c7
slavahokage <v_smirnov@ikantam.com>
2019-08-19 15:25:57 +0300

    create validation-feature

---
+++ core/Validation/Validator.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Core\Validation;
+
+class Validator implements RulesContract
+{
+    protected $fields;
+
+    protected $rules = [];
+
+    protected $brokenRules;
+
+    public function __construct($fields, $rules)
+    {
+        $this->fields = $fields;
+        $this->rules = $rules;
+
+        $this->parseRules();
+    }
+
+    protected function parseRules()
+    {
+        foreach ($this->rules as $ruleKey => $ruleValue) {
+            $currentRules = explode('|', $ruleValue);
+            foreach ($currentRules as $rule) {
+                if (method_exists($this, $rule)) {
+                    if (!call_user_func_array([$this, $rule], [[$ruleKey => $ruleValue], $this->fields])) {
+                        $this->brokenRules[$ruleKey][] = "rule $rule is broken for field ($ruleKey)";
+                    }
+                } else {
+                    throw new \Exception("Not illegal rule ($rule)");
+                }
+            }
+        }
+    }
+
+    public function isFail()
+    {
+        return !empty($this->brokenRules);
+    }
+
+    public function getBrokenRules()
+    {
+        return $this->brokenRules;
+    }
+
+    public function required($checkedField, $fieldsToCheck)
+    {
+        list($checkedField) = array_keys($checkedField);
+
+        return array_key_exists($checkedField, $fieldsToCheck);
+    }
+
+    public function filled($checkedField, $fieldsToCheck)
+    {
+        list($checkedField) = array_keys($checkedField);
+
+        return !empty($fieldsToCheck[$checkedField]);
+    }
+}
+
